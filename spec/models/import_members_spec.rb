0798877ebd1d92da3e05ffe28ed6a5fb70fe4a55
Paul Morganthall <paul@morganthall.com>
2019-01-04 15:19:09 -0500

    Outline the features of member import.

---
+++ spec/models/import_members_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "importing members" do
+  describe "basic import requirements" do
+    it "creates a valid member"
+    it "prohibits an invalid member"
+    it "ingests all specified fields"
+  end
+
+  describe "birthday" do
+    it "combines month, day, and year into birthday"
+    it "adds default year to birthdays if none was specified"
+  end
+
+  describe "directory preferences" do
+    it "represents interests as an opt-in list"
+    it "observes member requests to leave info out of directory"
+  end
+
+  describe "member newsletter preferences" do
+    it "records request for attachment"
+    it "records request for link-only"
+    it "records request for no email newsletter"
+  end
+end
