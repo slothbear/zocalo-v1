d8b5183a33b381be1f2edea7c9eb9fcf6dad9371
Paul Morganthall <paul@morganthall.com>
2019-01-17 15:53:16 -0500

    Display active (and recently expired) Members.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,7 @@
 class Member < ApplicationRecord
   validates :last_name, presence: true
+  scope :active, -> { Member.where(
+    'expire_on > ?', 2.months.ago.at_beginning_of_month) }
 
   ALL_DIRECTORY_ITEMS = %w[
     first_name
