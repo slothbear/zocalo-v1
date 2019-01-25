798a869fc8d89b74aac05597801f9ddb6763a74a
Paul Morganthall <paul@morganthall.com>
2019-01-25 16:01:03 -0500

    Show payments in reverse order by default.

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,21 +1,7 @@
 module Admin
   class PaymentsController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = Payment.
-    #     page(params[:page]).
-    #     per(10)
-    # end
-
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   Payment.find_by!(slug: param)
-    # end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
+    def scoped_resource
+      Payment.order(paid_on: "desc")
+    end
   end
 end
